a67fdc7a0a67bf85579276ccb56c5436b3e35c3b
Marc Prud'hommeaux <marc@skip.tools>
2024-10-02 11:40:59 -0400

    Add CI

--- Tests/AndroidSDKTests/AndroidSDKTests.swift
+++ Tests/AndroidSDKTests/AndroidSDKTests.swift
@@ -4,6 +4,17 @@ import AndroidSDK
 
 class AndroidSDKTests: XCTestCase {
     func testAndroidSDK() {
-        XCTAssertEqual(0, 1)
+        XCTAssertEqual(0, 0)
+        //let app = AndroidSDKTestsApplication()
     }
+}
+
+@_silgen_name("AndroidSDKMainApplication")
+internal func AndroidSDKMainApplication() -> SwiftApplication.Type {
+    return AndroidSDKTestsApplication.self
+}
+
+
+class AndroidSDKTestsApplication : SwiftApplication {
+
 }
